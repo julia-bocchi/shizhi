af16d38620ee89ba27e86d2f356c2603ed953329
chen <1608956857@qq.com>
2026-04-13 21:30:52 +0800

    feat():

---
+++ SZ-server/src/main/java/com/chen/server/SzFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.chen.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzFrameworkApplication.class, args);
+    }
+
+}
---
+++ SZ-server/src/test/java/com/chen/server/SzFrameworkApplication.java
@@ -0,0 +1,12 @@
+package  com.chen.server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzFrameworkApplication.class, args);
+    }
+
+}
